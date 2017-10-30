175e62577c62b8b5d2ec470519283e0d68d6f259
thehawkguy <p.u.moeller@gmail.com>
2017-10-30 07:41:01 +0100

    really minor changes to powerups

--- powerup.lua
+++ powerup.lua
@@ -1,12 +1,12 @@
 require "entity"
 PowerUp = Entity:extend()
 
-function PowerUp:new( ... )
-    -- body
-    self x = math.random(Settings.xRand1, xRand2)
-    self y = Settings.WindowHeight / 2
+function PowerUp:new(x, y, k)
+    -- powerups sollen in mitte der fensterhöhe gespawnt werden, zufällig zur fensterbreite; sie bekommen ein bild. sie haben eine art (k)
+    self.x = math.random(Settings.xRand1, xRand2)
+    self.y = Settings.WindowHeight / 2
     self.image = love.graphics.newImage(imagePowerUp)
-    
+    self.k = k
 
 end
 
