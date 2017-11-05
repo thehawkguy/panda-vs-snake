67a6adc1b20cf7b7141516dc3ae016d100e2d50d
thehawkguy <p.u.moeller@gmail.com>
2017-11-05 16:41:52 +0100

    changed title

--- conf.lua
+++ conf.lua
@@ -1,5 +1,5 @@
 function love.conf(t)
-  t.window.title = "Panda vs Snake v0.0.1"
+  t.window.title = "Panda vs Snake v1.0"
   t.window.icon = "panda.png"
   t.window.width = 1280
   t.window.height = 720
